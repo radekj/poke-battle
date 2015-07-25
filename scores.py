bd0b3a9c7ea27b610ef013afe42214d922282f92
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-07-25 12:00:45 +0200

    Start implementing ScoreService

---
+++ scores.py
@@ -0,0 +1,12 @@
+from nameko.rpc import rpc, RpcProxy
+
+class ScoreService:
+    name = 'score_service'
+
+    player_service = RpcProxy('players_service')
+
+    @rpc
+    def leaderboard(self):
+        players = self.player_service.players()
+        player_scores = [(player.name, player.score) for player in players]
+        return sorted(player_scores, key=lambda player: player[1], reverse=True)
