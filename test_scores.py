1061883196b88b3ebc6e88fff27411910703523d
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-07-25 17:25:13 +0200

    Tests passing with py.test
    Note: the last test is not passing because there's no rabbit config

--- test_scores.py
+++ test_scores.py
@@ -1,3 +1,4 @@
+from mock import MagicMock
 from nameko.containers import ServiceContainer
 from nameko.testing.services import worker_factory
 from nameko.standalone.events import event_dispatcher
@@ -5,19 +6,18 @@ from nameko.testing.services import entrypoint_waiter
 
 from scores import ScoreService
 
-
 def scores_service_with_players():
+    players = [MagicMock(name='player2', score=10), MagicMock(name='player1', score=20)]
     service = worker_factory(ScoreService)
-    service.player_rpc.new_player('player1')
-    service.player_rpc.new_player('player2')
-    service.player_rpc.get_player(1).add_score(10)
-    service.player_rpc.get_player(2).add_score(20)
+    service.player_rpc.get_players.side_effect = lambda: players
+    service.player_rpc.get_player.side_effect = lambda p: players[p - 1]
     return service
 
 def test_leaderboard():
     service = scores_service_with_players()
-
-    assert service.leaderboard() == [('player2', 20), ('player1', 10)]
+    leaderboard = service.leaderboard()
+    assert leaderboard[0] == (service.player_rpc.get_players()[1].name, 20)
+    assert leaderboard[1] == (service.player_rpc.get_players()[0].name, 10)
 
 def test_empty_leaderboard():
     service = worker_factory(ScoreService)
@@ -25,11 +25,11 @@ def test_empty_leaderboard():
 
 def test_update_scores_on_battle_finished():
     service = scores_service_with_players()
-    winner = service.player_rpc.get_player(1)
-    loser = service.player_rpc.get_player(2)
+    winner_initial_score = service.player_rpc.get_player(1).score
+    loser_initial_score = service.player_rpc.get_player(2).score
     service.update_players_score([0, 1, 2])
-    assert service.player_rpc.get_player(1) == winner.score + 2
-    assert service.player_rpc.get_player(2) == winner.score + 1
+    service.player_rpc.get_player(1).add_score.assert_called_once_with(2)
+    service.player_rpc.get_player(2).add_score.assert_called_once_with(1)
 
 def test_update_is_called_when_battle_finishes(rabbit_config):
     container = ServiceContainer(ScoreService, rabbit_config)
