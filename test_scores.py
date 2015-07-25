72bda470adbc4d56599c1af8fb1132e6a01a91df
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-07-25 15:56:14 +0200

    Remove old unittest class

--- test_scores.py
+++ test_scores.py
@@ -2,18 +2,17 @@ from scores import ScoreService
 from nameko.testing.services import worker_factory
 
 
-class TestScores(unittest.TestCase):
-    def test_leaderboard(self):
-        service = worker_factory(ScoreService)
-        service.player_rpc.new_player('player1')
-        service.player_rpc.new_player('player2')
-        service.player_rpc.get_player(1).add_score(10)
-        service.player_rpc.get_player(2).add_score(20)
+def test_leaderboard(self):
+    service = worker_factory(ScoreService)
+    service.player_rpc.new_player('player1')
+    service.player_rpc.new_player('player2')
+    service.player_rpc.get_player(1).add_score(10)
+    service.player_rpc.get_player(2).add_score(20)
 
-        self.assertEquals(
-            service.leaderboard(),
-            [('player2', 20), ('player1', 10)])
+    self.assertEquals(
+        service.leaderboard(),
+        [('player2', 20), ('player1', 10)])
 
-    def test_empty_leaderboard(self):
-        service = worker_factory(ScoreService)
-        self.assertEquals(service.leaderboard(), [])
+def test_empty_leaderboard(self):
+    service = worker_factory(ScoreService)
+    self.assertEquals(service.leaderboard(), [])
