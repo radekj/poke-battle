a9777ff5bc75e4d28f325bbe00601180f6b5ce77
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-07-26 10:30:41 +0200

    Refactor the players API to use the DB

--- pokebattle/db.py
+++ pokebattle/db.py
@@ -9,4 +9,11 @@ class Pokemon(Base):
     __tablename__ = 'pokemon'
     id = sa.Column(sa.Integer, primary_key=True)
     pokemon_name = sa.Column(sa.String)
-    user_id = sa.Column(sa.Integer)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey('players.id'))
+
+
+class Player(Base):
+    __tablename__ = 'players'
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String)
+    score = sa.Column(sa.Integer, default=0)
--- tests/test_players.py
+++ tests/test_players.py
@@ -1,24 +1,38 @@
 import pytest
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from nameko.testing.services import worker_factory
 
+from pokebattle.db import Base
 from pokebattle.players import PlayersService
 
 
-def test_players():
-    players_service = worker_factory(PlayersService)
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite://')
+    Base.metadata.create_all(engine)
+    session_cls = sessionmaker(bind=engine)
+    session = session_cls()
+    return session
+
+
+@pytest.fixture
+def service(session):
+    return worker_factory(PlayersService, session=session)
+
+def test_players(service):
 
     first_name = 'Paul'
-    first_player = players_service.new_player(first_name)
-    players_service.new_player('Mike')
+    first_player = service.new_player(first_name)
+    service.new_player('Mike')
 
     assert first_player.name == first_name
-    assert players_service.get_player(first_player.uuid).name == \
+    assert service.get_player(first_player.id).name == \
         first_player.name
-    assert len(players_service.get_players()) == 2
+    assert len(service.get_players()) == 2
 
     try:
-        players_service.new_player(first_name)
+        service.new_player(first_name)
         pytest.fail()
     except RuntimeError:
         pass
